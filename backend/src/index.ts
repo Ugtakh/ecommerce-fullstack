c81c1a05b8990180726e4fdd17ebec070ed5ba9c
Ugtakh <ugtakhbayars@gmail.com>
2024-09-24 13:44:45 +0800

    add email

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,24 +6,28 @@ import { Resend } from "resend";
 
 import authRoute from "./routes/auth-route";
 import { connectDB } from "./config/db";
+import { generateHtmlTemplate } from "./utils/generateHtmlTemplate";
 
 const PORT: string = process.env.PORT || "";
 const MONGO_URI = process.env.MONGO_URI || "";
 
 // express application object uusgeh
 const app = express();
-const resend = new Resend(process.env.RS_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 //middlewares
 app.use(express.json());
 app.use("/api/v1/auth", authRoute);
 
 app.get("/", async (req: Request, res: Response) => {
+  const rndOtp = Math.floor(Math.random() * 10_000)
+    .toString()
+    .padStart(4, "0");
   const { data, error } = await resend.emails.send({
     from: "Acme <onboarding@resend.dev>",
     to: ["ugtakhbayar.pico@gmail.com"],
-    subject: "Hello World",
-    html: "<strong>It works!</strong>",
+    subject: "Сайн байна уу",
+    html: generateHtmlTemplate(rndOtp),
   });
   if (error) {
     console.error("EMAIL_ERR", { error });
